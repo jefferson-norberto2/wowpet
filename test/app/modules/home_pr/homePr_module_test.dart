548524956304d42b1295dc3a9d4282ee7a1a7f89
jefferson-norberto2 <jmn@cin.ufpe.br>
2023-05-21 14:45:22 -0300

    adjust test file

--- test/app/modules/home_pr/homePr_module_test.dart
+++ test/app/modules/home_pr/homePr_module_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:modular_test/modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:wowpet/app/modules/home_pr/home_pr_module.dart';
